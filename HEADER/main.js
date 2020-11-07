15e4948a99932b332434eb4ee427bb2400b184e8
dhwns <dhwns8058@naver.com>
2020-11-08 01:20:59 +0900

    반응형 헤더 만들기
    DreamCoding

---
+++ HEADER/main.js
@@ -0,0 +1,14 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn')
+const menu = document.querySelector('.navbar__menu')
+const icons = document.querySelector('.navbar__icons')
+
+toggleBtn.addEventListener("click", () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
+
+
+/*var t = document.getElementById('target');
+t.addEventListener('click', function(event){
+    alert('Hello world, '+event.target.value);
+}); */
